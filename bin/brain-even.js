d53fb6d904f2061159eb92d9e8a39a349ac81244
nityulam <nityulam@mail.ru>
2024-08-15 11:37:39 +0700

    created brain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import {
+  /*  findOutName, */
+  /* findEvenNumber, */
+  /* showingNumber, */
+  userAnswer,
+} from '../src/game-even.js';
+
+console.log('Welcome to the Brain Games!');
+
+/*  findOutName(); */
+
+/* findEvenNumber(); */
+
+/* showingNumber(); */
+
+userAnswer();
