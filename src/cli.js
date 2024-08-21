fda14107d0e31bf44653d8113062b5bbd38fe9da
nityulam <nityulam@mail.ru>
2024-08-21 15:02:01 +0700

    fixed cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,11 @@
 import readlineSync from 'readline-sync';
 
+console.log('Welcome to the Brain Games!');
+
 const findOutName = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
 };
+
+console.log(`Hello, ${userName}!`);
 
 export default findOutName;
