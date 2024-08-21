5c4793659e85c41d0962c7afb943ea1a602da433
nityulam <nityulam@mail.ru>
2024-08-21 15:00:41 +0700

    fixed brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,17 +1,4 @@
 #!/usr/bin/env node
-import {
-  /*  findOutName, */
-  /* findEvenNumber, */
-  /* showingNumber, */
-  userAnswer,
-} from '../src/game-even.js';
+import gameEven from '../src/games/game-even.js';
 
-console.log('Welcome to the Brain Games!');
-
-/*  findOutName(); */
-
-/* findEvenNumber(); */
-
-/* showingNumber(); */
-
-userAnswer();
+gameEven();
