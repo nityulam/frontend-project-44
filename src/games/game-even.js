ab523c38ba8d92097871a6b4b2b854b7d901f6ec
nityulam <nityulam@mail.ru>
2024-08-28 18:28:46 +0700

    fixed  game-even.js

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -19,35 +19,3 @@ const correctAnswer = () => {
 export default () => {
   generalLogicForGames(ruleOfGame, correctAnswer);
 };
-/*
-const userAnswer = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  for (let i = 0; i < 3; i += 1) {
-    const randomNumber = Math.floor(Math.random() * 100);
-    console.log(`Question: ${randomNumber}`);
-
-    const calculatingRemainder = randomNumber % 2;
-
-    const answer = readlineSync.question('Your answer: ');
-    if (calculatingRemainder === 0 && answer.toLowerCase() === 'yes') {
-      console.log('Correct!');
-    } else if (calculatingRemainder !== 0 && answer.toLowerCase() === 'no') {
-      console.log('Correct!');
-    } else if (calculatingRemainder === 0 && answer.toLowerCase() !== 'yes') {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    } else if (calculatingRemainder !== 0 && answer.toLowerCase() !== 'no') {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
-
-
-*/
