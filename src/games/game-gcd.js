410f47928f64b17f8d14a2d9af5ede4204133fc3
nityulam <nityulam@mail.ru>
2024-09-02 15:05:45 +0700

    fixed game-gcd.js

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -1,42 +1,39 @@
 import generalLogicForGames from '../index.js';
+import getRandomNumber from '../random-number.js';
 
 const ruleOfGame = 'Find the greatest common divisor of given numbers.';
 
-const randomNumber = (min, max) => {
-  const random = Math.floor(Math.random() * (max - min) + min);
-  return random;
-};
-
-const randomNumbers = () => {
-  const num1 = randomNumber(1, 100);
-  const num2 = randomNumber(1, 100);
+const getGreatestCommonDivisor = (num1, num2) => {
   const minNum = Math.min(num1, num2);
-
-  const mathcExpressio = `${num1} ${num2}`;
   let result;
-
   for (let i = minNum; i > 0; i -= 1) {
     const mathOperation = num1 % i;
     const mathOperation2 = num2 % i;
-    // console.log('minNum =', minNum, 'i =', i, 'num1 =', num1, 'num2 =', num2);
-    // console.log('mathOper =', mathOperation, 'mathOper2 =', mathOperation2);
+
     if (mathOperation === 0 && mathOperation2 === 0) {
       result = i;
-      // У result изменил тип на строковый, в связи с тем, что даваемый ответ игроком,
-      // является строковым типом. Как сделать иначе не разобрался.
-      return [mathcExpressio, String(result)];
+      return result;
     }
     result = 0;
   }
-  return [mathcExpressio, String(result)];
+  return result;
 };
 
-const correctAnswer = () => {
-  const [question, rightAnswer] = randomNumbers();
+const passingNecessaryElements = () => {
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
+  const question = `${num1} ${num2}`;
+  const rightAnswer = getGreatestCommonDivisor(num1, num2);
+
+  return [question, String(rightAnswer)];
+};
+
+const getQuestionAndCorrectAnswer = () => {
+  const [question, rightAnswer] = passingNecessaryElements();
 
   return [question, rightAnswer];
 };
 
 export default () => {
-  generalLogicForGames(ruleOfGame, correctAnswer);
+  generalLogicForGames(ruleOfGame, getQuestionAndCorrectAnswer);
 };
