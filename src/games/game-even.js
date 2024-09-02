b85b23a985d50cedc22d57c533dfae30f3cdb27f
nityulam <nityulam@mail.ru>
2024-09-02 15:05:05 +0700

    fixed game-even.js

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,21 +1,17 @@
 import generalLogicForGames from '../index.js';
+import getRandomNumber from '../random-number.js';
 
 const ruleOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (randNum) => randNum % 2 === 0;
+const isEvenNumber = (randNum) => randNum % 2 === 0;
 
-const randomNumber = () => {
-  const result = Math.floor(Math.random() * 100);
-  return result;
-};
-
-const correctAnswer = () => {
-  const question = randomNumber();
-  const rightAnswer = isEven(question) === true ? 'yes' : 'no';
+const getQuestionAndCorrectAnswer = () => {
+  const question = getRandomNumber();
+  const rightAnswer = isEvenNumber(question) === true ? 'yes' : 'no';
 
   return [question, rightAnswer];
 };
 
 export default () => {
-  generalLogicForGames(ruleOfGame, correctAnswer);
+  generalLogicForGames(ruleOfGame, getQuestionAndCorrectAnswer);
 };
