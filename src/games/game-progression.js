434c21d9c72acda4f229afb704451f4541526cb1
nityulam <nityulam@mail.ru>
2024-09-02 15:06:40 +0700

    fixed game-progression.js

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,50 +1,33 @@
 import generalLogicForGames from '../index.js';
+import getRandomNumber from '../random-number.js';
 
 const ruleOfGame = 'What number is missing in the progression?';
 
-const randomNumber = (min, max = 100) => {
-  const random = Math.floor(Math.random() * (max - min) + min);
-  return random;
-};
+const getArithmeticProgression = () => {
+  const lengthProgression = getRandomNumber(4, 10);
 
-const arithmeticProgression = () => {
-  const arrayLength = randomNumber(4, 10);
-
-  // Немного длинная переменная :))) (понимаю что не очень хороший вариант)
-  let beginningOfArrayWithItsSubsequentIncrease = randomNumber(0, 50);
-  const stepIncreasingProgression = randomNumber(2, 11);
+  let beginningOfArrayWithItsSubsequentIncrease = getRandomNumber(0, 50);
+  const stepIncreasingProgression = getRandomNumber(2, 11);
   const arrayOfArithProgression = [];
 
-  // console.log('Шаг прогрессии', stepIncreasingProgression);
-
-  for (let i = 0; i <= arrayLength; i += 1) {
+  for (let i = 0; i <= lengthProgression; i += 1) {
     beginningOfArrayWithItsSubsequentIncrease += stepIncreasingProgression;
     arrayOfArithProgression.push(beginningOfArrayWithItsSubsequentIncrease);
   }
 
-  const indexArrayBeReplaced = randomNumber(0, arrayOfArithProgression.length);
+  const indexArrayBeReplaced = getRandomNumber(0, arrayOfArithProgression.length);
   const hiddenArrayElement = arrayOfArithProgression[indexArrayBeReplaced];
   arrayOfArithProgression[indexArrayBeReplaced] = '..';
-
-  /*
-  console.log(
-    typeof arrayOfArithProgression,
-    arrayOfArithProgression.join(' ')
-  );
-  */
-
-  // console.log('Индексный массив должен быть заменен', indexArrayBeReplaced);
-  // console.log('Скрытый элемент массива', hiddenArrayElement);
 
   return [arrayOfArithProgression.join(' '), String(hiddenArrayElement)];
 };
 
-const correctAnswer = () => {
-  const [question, rightAnswer] = arithmeticProgression();
+const getQuestionAndCorrectAnswer = () => {
+  const [question, rightAnswer] = getArithmeticProgression();
 
   return [question, rightAnswer];
 };
 
 export default () => {
-  generalLogicForGames(ruleOfGame, correctAnswer);
+  generalLogicForGames(ruleOfGame, getQuestionAndCorrectAnswer);
 };
