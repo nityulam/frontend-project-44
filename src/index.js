3d95bcc52a2e2d8e18016ce17ed348b43c557586
nityulam <nityulam@mail.ru>
2024-09-02 15:01:06 +0700

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import readlineSync from 'readline-sync';
 
-// ruleOfGame - показываем правило игры (правило прописываем в файле с конкретной игрой)
-// correctAnswer содержит в себе две константы:
-// 1. question - цифру (цифра является вопросом) 2. rightAnswer - правильный ответ
-
-const generalLogicForGames = (ruleOfGame, correctAnswer) => {
+const playGame = (ruleOfGame, correctAnswer) => {
   console.log('Welcome to the Brain Games!');
 
   const userName = readlineSync.question('May I have your name? ');
@@ -13,7 +9,9 @@ const generalLogicForGames = (ruleOfGame, correctAnswer) => {
 
   console.log(ruleOfGame);
 
-  for (let i = 0; i < 3; i += 1) {
+  const numberRoundsInGames = 3;
+
+  for (let i = 0; i < numberRoundsInGames; i += 1) {
     const [question, rightAnswer] = correctAnswer();
     console.log(`Question: ${question}`);
 
@@ -22,9 +20,6 @@ const generalLogicForGames = (ruleOfGame, correctAnswer) => {
     if (answer.toLowerCase().replaceAll(' ', '') === rightAnswer) {
       console.log('Correct!');
     } else {
-      // Не могу разобраться почему при сохранении, автоматически переносит
-      // скобку с точкой с запятой на другую строку.Какое правило описывает данное поведение?
-      // Дело было в настройках макс длины строки Prettier (поменял в VSC)
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
@@ -33,4 +28,4 @@ const generalLogicForGames = (ruleOfGame, correctAnswer) => {
   console.log(`Congratulations, ${userName}!`);
 };
 
-export default generalLogicForGames;
+export default playGame;
