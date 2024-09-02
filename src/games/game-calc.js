fc7ddf31b593c84413342fdf42e9ff212d4d2214
nityulam <nityulam@mail.ru>
2024-09-02 15:05:30 +0700

    fixed game-calc.js

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -1,27 +1,16 @@
 import generalLogicForGames from '../index.js';
+import getRandomNumber from '../random-number.js';
 
 const ruleOfGame = 'What is the result of the expression?';
 
-const randomNumber = (min, max) => {
-  const random = Math.floor(Math.random() * (max - min) + min);
-  return random;
-};
-
-// Генерируем математический оператор
-const randomMathOperator = () => {
+const getMathematicalOperator = () => {
   const operators = ['-', '+', '*'];
-  const randomIndex = randomNumber(0, operators.length);
+  const randomIndex = getRandomNumber(0, operators.length);
   return operators[randomIndex];
 };
 
-const randomMathcExpression = () => {
-  const num1 = randomNumber(1, 10);
-  const num2 = randomNumber(1, 10);
-  const operator = randomMathOperator();
-
-  const mathcExpressio = `${num1} ${operator} ${num2}`;
+const creatingExpressionForQuestion = (num1, num2, operator) => {
   let result;
-
   switch (operator) {
     case '-':
       result = num1 - num2;
@@ -34,55 +23,25 @@ const randomMathcExpression = () => {
       break;
 
     default:
-    // do nothing
   }
-
-  // У result изменил тип на строковый, в связи с тем, что даваемый ответ игроком,
-  // является строковым типом. Как сделать иначе не разобрался.
-  return [mathcExpressio, String(result)];
+  return result;
 };
 
-const correctAnswer = () => {
-  const [question, rightAnswer] = randomMathcExpression();
+const passingNecessaryElements = () => {
+  const num1 = getRandomNumber(1, 10);
+  const num2 = getRandomNumber(1, 10);
+  const operator = getMathematicalOperator();
+  const question = `${num1} ${operator} ${num2}`;
+  const rightAnswer = creatingExpressionForQuestion(num1, num2, operator);
+  return [question, String(rightAnswer)];
+};
+
+const getQuestionAndCorrectAnswer = () => {
+  const [question, rightAnswer] = passingNecessaryElements();
 
   return [question, rightAnswer];
 };
 
 export default () => {
-  generalLogicForGames(ruleOfGame, correctAnswer);
+  generalLogicForGames(ruleOfGame, getQuestionAndCorrectAnswer);
 };
-
-/*
-
-const calculatorGame = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('What is the result of the expression?');
-
-  for (let i = 0; i < 3; i += 1) {
-    const randomNumber1 = Math.floor(Math.random() * 10);
-    const randomNumber2 = Math.floor(Math.random() * 10);
-    console.log(`Question: ${randomNumber1} + ${randomNumber2}`);
-
-    const calculateMathOperation = randomNumber1 + randomNumber2;
-
-    const answer = readlineSync.question('Your answer: ');
-
-    if (calculateMathOperation === Number(answer)) {
-      console.log('Correct!');
-    } else if (calculateMathOperation !== Number(answer)) {
-      console.log(
-        `'${answer}' is wrong answer ;(. Correct answer was '${calculateMathOperation}'.`
-      );
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    } else {
-      console.log('The answer is not correct');
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
-
-export { calculatorGame };
-*/
