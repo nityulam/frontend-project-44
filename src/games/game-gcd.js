1e14bc76cb2e9ef81a1df474ca4f6de443676bbd
nityulam <nityulam@mail.ru>
2024-09-03 16:48:20 +0700

    fixed game-gcd.js

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -1,5 +1,5 @@
 import generalLogicForGames from '../index.js';
-import getRandomNumber from '../random-number.js';
+import getRandomNumber from '../utils.js';
 
 const ruleOfGame = 'Find the greatest common divisor of given numbers.';
 
