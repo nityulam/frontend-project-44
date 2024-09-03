c6d7643da337cdb7db8bebf915622f048754868d
nityulam <nityulam@mail.ru>
2024-09-03 16:47:18 +0700

    fixed game-calc.js

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -1,5 +1,5 @@
 import generalLogicForGames from '../index.js';
-import getRandomNumber from '../random-number.js';
+import getRandomNumber from '../utils.js';
 
 const ruleOfGame = 'What is the result of the expression?';
 
@@ -9,39 +9,30 @@ const getMathematicalOperator = () => {
   return operators[randomIndex];
 };
 
-const creatingExpressionForQuestion = (num1, num2, operator) => {
-  let result;
+const getExpressionForQuestion = (num1, num2, operator) => {
   switch (operator) {
     case '-':
-      result = num1 - num2;
-      break;
+      return num1 - num2;
     case '+':
-      result = num1 + num2;
-      break;
+      return num1 + num2;
+
     case '*':
-      result = num1 * num2;
-      break;
+      return num1 * num2;
 
     default:
+      throw new Error(`Unknown operator: '${operator}'!`);
   }
-  return result;
 };
 
-const passingNecessaryElements = () => {
+const getQuestionAndAnswer = () => {
   const num1 = getRandomNumber(1, 10);
   const num2 = getRandomNumber(1, 10);
   const operator = getMathematicalOperator();
   const question = `${num1} ${operator} ${num2}`;
-  const rightAnswer = creatingExpressionForQuestion(num1, num2, operator);
+  const rightAnswer = getExpressionForQuestion(num1, num2, operator);
   return [question, String(rightAnswer)];
 };
 
-const getQuestionAndCorrectAnswer = () => {
-  const [question, rightAnswer] = passingNecessaryElements();
-
-  return [question, rightAnswer];
-};
-
 export default () => {
-  generalLogicForGames(ruleOfGame, getQuestionAndCorrectAnswer);
+  generalLogicForGames(ruleOfGame, getQuestionAndAnswer);
 };
